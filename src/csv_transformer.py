b4addf39a4889f81f4391f1317a6b0b31dd0589e
arthur tung <aarthur.tung@gmail.com>
2025-08-13 14:42:08 -0300

    Refactor CSV paths and enhance data reading in transformer and previewer scripts

--- src/csv_transformer.py
+++ src/csv_transformer.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 csv_cursos_paths = ['data/ces/microdata_20' + str(i) + '/dados/MICRODADOS_CADASTRO_CURSOS_20' + str(i) + '.csv' for i in range(19, 24)]
-csv_IESs_paths = ['data/ces/microdata_20' + str(i) + '/dados/MICRODADOS_CADASTRO_IES_20' + str(i) + '.csv' for i in range(19, 24)]
+csv_IES_paths = ['data/ces/microdata_20' + str(i) + '/dados/MICRODADOS_CADASTRO_IES_20' + str(i) + '.csv' for i in range(19, 24)]
+csv_enem_paths = ['data/enem/microdata_20' + str(i) + '/DADOS/MICRODADOS_ENEM_20' + str(i) + '.csv' for i in range(23, 24)]
 
 def read_csvs(paths):
     dfs = []
@@ -30,13 +31,20 @@ def transform_cursos_data(dfs):
 def main():
     print("Iniciando leitura dos arquivos...")
     cursos_dfs = read_csvs(csv_cursos_paths)
-    ies_dfs = read_csvs(csv_IESs_paths)
+    cursos_dfs = pd.concat(cursos_dfs, ignore_index=True)
+    cursos_dfs.to_csv('data/transformed_data/cursos.csv', index=False, sep=';')
+    ies_dfs = read_csvs(csv_IES_paths)
+    ies_dfs = pd.concat(ies_dfs, ignore_index=True)
+    ies_dfs.to_csv('data/transformed_data/ies.csv', index=False, sep=';')
+    enem_dfs = read_csvs(csv_enem_paths)
+    enem_dfs = pd.concat(enem_dfs, ignore_index=True)
+    enem_dfs.to_csv('data/transformed_data/enem.csv', index=False, sep=';')
 
-    print("Escrevendo dados transformados...")
+    '''print("Escrevendo dados transformados...")
     cursos_sp = transform_cursos_data(cursos_dfs)
     cursos_sp.to_csv('data/transformed_data/cursos_sp.csv', index=False, sep=';')    
     ies_sp = transform_cursos_data(ies_dfs)
-    ies_sp.to_csv('data/transformed_data/ies_sp.csv', index=False, sep=';')
+    ies_sp.to_csv('data/transformed_data/ies_sp.csv', index=False, sep=';')'''
 
 if __name__ == "__main__":
     main()
--- src/previewer.py
+++ src/previewer.py
@@ -1,7 +1,15 @@
 import pandas as pd
 
-csv_cursos = pd.read_csv('data/transformed_data/cursos_sp.csv', sep=';', encoding='latin1', low_memory=False)
-csv_ies = pd.read_csv('data/transformed_data/ies_sp.csv', sep=';', encoding='latin1', low_memory=False)
+csv_cursos = pd.read_csv('data/transformed_data/cursos.csv', sep=';', encoding='latin1', low_memory=False)
+csv_ies = pd.read_csv('data/transformed_data/ies.csv', sep=';', encoding='latin1', low_memory=False)
+csv_enem = pd.read_csv('data/transformed_data/enem.csv', sep=';', encoding='latin1', low_memory=False)
 
-print('as colunas do csv de cursos sao:',csv_cursos.columns)
-print('as colunas do csv de ies sao:',csv_ies.columns)
+print('as colunas do csv de cursos sao:')
+for col in csv_cursos.columns:
+    print(col)
+print('\nas colunas do csv de ies sao:')
+for col in csv_ies.columns:
+    print(col)
+print('\nas colunas do csv do enem sao:')
+for col in csv_enem.columns:
+    print(col)
